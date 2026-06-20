bfab4029c6846a1fc558cb2d09041a23ff4ea60f
Russell Bley <russellbley@me.com>
2026-06-20 12:51:52 -0400

    Updated functionality

--- crawl.py
+++ crawl.py
@@ -1,4 +1,18 @@
-from urllib import urlparse
+from urllib.parse import urlparse
 
-def normalize_url():
-    
+
+def normalize_url(url) -> str:
+    """
+    Normalize a URL by parsing it and reconstructing it in a standard format.
+
+    :param url: The URL to be normalized.
+    :return: A normalized URL string.
+    """
+
+    response = urlparse(url)
+    netloc = response.netloc.lower() # domain
+    path = response.path.lower().rstrip('/') # subdomain
+    # scheme = response.scheme.lower() # HTTP or HTTPS
+
+    new_url = f'{netloc}{path}'
+    return new_url
--- test_crawl.py
+++ test_crawl.py
@@ -1,12 +1,14 @@
 import unittest
 from crawl import normalize_url
 
-class TestCrawl(unittest, TestCase):
+class TestCrawl(unittest.TestCase):
     def test_normalize_url(self):
-        input_url = 'https://www.boot.dev/blog/path'
-        actual = normalize_url(input_url)
-        expected = 'www.boot.dev/blog/path'
-        self.assertEqual(actual, expected)
+        self.assertEqual(normalize_url('https://www.boot.dev/blog/path'), 'www.boot.dev/blog/path')
+    def test_normalize_url_with_slash(self):
+        # The runner automatically finds and runs this too!
+        self.assertEqual(normalize_url("https://example.com/path/"), "example.com/path")
+    def test_normalize_url_without_slash(self):
+        self.assertEqual(normalize_url("http://example.com"), "example.com")
 
 if __name__ == '__main__':
     unittest.main()
