3fcb02544f6af66b1fc0da10822ed27229b1da52
Russell Bley <russellbley@me.com>
2026-06-20 13:19:07 -0400

    Added HTML find()

--- crawl.py
+++ crawl.py
@@ -1,5 +1,7 @@
 from urllib.parse import urlparse
 
+from bs4 import BeautifulSoup, Tag
+
 
 def normalize_url(url) -> str:
     """
@@ -16,3 +18,21 @@ def normalize_url(url) -> str:
 
     new_url = f'{netloc}{path}'
     return new_url
+
+
+def get_heading_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, 'html.parser')
+
+    h_tag = soup.find('h1')
+    return h_tag.get_text(strip=True) if isinstance(h_tag, Tag) else ""
+
+def get_first_paragraph_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, 'html.parser')
+
+    main_tag = soup.find('main')
+    if isinstance(main_tag, Tag):
+        p_tag = main_tag.find('p')
+    else:
+        p_tag = soup.find('p')
+
+    return p_tag.get_text(strip=True) if isinstance(p_tag, Tag) else ""
--- test_crawl.py
+++ test_crawl.py
@@ -1,16 +1,32 @@
 import unittest
-from crawl import normalize_url
+from crawl import normalize_url, get_heading_from_html, get_first_paragraph_from_html
 
 class TestCrawl(unittest.TestCase):
     def test_normalize_url(self):
         self.assertEqual(normalize_url('https://www.boot.dev/blog/path'), 'www.boot.dev/blog/path')
     def test_normalize_url_with_slash(self):
-        # The runner automatically finds and runs this too!
         self.assertEqual(normalize_url("https://example.com/path/"), "example.com/path")
     def test_normalize_url_without_slash(self):
         self.assertEqual(normalize_url("http://example.com"), "example.com")
     def test_normalize_url_with_slashes(self):
         self.assertEqual(normalize_url("http://example.com/path//"), "example.com/path")
+
+    def test_get_heading_from_html_basic(self):
+        input_body = '<html><body><h1>Test Title</h1></body></html>'
+        actual = get_heading_from_html(input_body)
+        expected = "Test Title"
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_from_html_main_priority(self):
+        input_body = '''<html><body>
+            <p>Outside paragraph.</p>
+            <main>
+                <p>Main paragraph.</p>
+            </main>
+        </body></html>'''
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "Main paragraph."
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
